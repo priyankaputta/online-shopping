9f489ae1f759b0ccf37da6524e4e235d46ef5927
Priyanka Putta <priyanka.putta@gmail.com>
2017-10-13 11:16:34 +0100

    My Project setup

---
+++ shoppingbackend/src/main/java/net/ppt/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.ppt.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
